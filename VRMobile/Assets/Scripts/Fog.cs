593b4cc6beaf645d2c5131d4e8e9cd74ffb6c982
Tomi Vähämäki <tomivaha@metropolia.fi>
2018-02-09 11:49:45 +0200

    Armageddon

---
+++ VRMobile/Assets/Scripts/Fog.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fog : MonoBehaviour
+{
+    public float minDensity;
+    public float maxDensity;
+    public float speed;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    RenderSettings.fogDensity = minDensity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (RenderSettings.fogDensity < maxDensity)
+	    {
+	        RenderSettings.fogDensity += speed * Time.deltaTime;
+	    }
+	}
+}
